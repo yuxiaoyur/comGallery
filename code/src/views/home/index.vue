57b3f9fadd6dcb03a8063bdca6686901b12d947b
CAL-lqq <18281618570@163.com>
2021-06-29 10:47:36 +0800

    增加基础内容

--- code/src/views/home/index.vue
+++ code/src/views/home/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="app-container">
+  <div class="app-container home-container">
     <!-- <div class="home-top con-fpmzd">广西联合奖惩系统</div> -->
     <!-- <div class="home-page"></div>
     <div class="home-title con-fhei">联合惩戒</div>
@@ -9,43 +9,25 @@
     <p class="home-content">
       《备忘录》共提出55项惩戒措施，分为八大类，第一类是对失信被执行人设立金融类机构的限制措施；第二类是对失信被执行人从事民商事行为的限制措施；第三类是对失信被执行人行业准入的限制措施，例如限制招录（聘）其为公务员或事业单位工作人员等；第四类是对失信被执行人担任重要职务的限制措施，例如限制担任金融机构的董事、监事、高级管理人员等；第五类是对失信被执行人享受优惠政策或荣誉的限制措施；第六类是对失信被执行人高消费及其他消费行为的限制措施，例如限制乘坐飞机、列车软卧、高铁，限制子女就读高收费私立学校等；第七类是对失信被执行人限制出境、定罪处罚的限制措施；第八类是协助查询和公示失信被执行人信息的措施。
     </p> -->
-
-      
+    <div class="home-title con-fpmzd color-304156">欢迎使用公共组件管理系统</div>
+    <i class="el-icon-info"></i>
   </div>
 </template>
 
 <script>
- 
+import Vue from 'vue';
+import {Icon} from 'element-ui';
+Vue.use(Icon);
+
+export default {}
 </script>
 
-<style scoped>
-  .home-page {
-    width: 940px;
-    height: 260px;
-    margin: 0 auto;
-    background: url('../../assets/images/home.png')no-repeat 100% 100%;
-    background-size: cover;
-  }
-  .home-top {
+<style  rel="stylesheet/scss" lang="scss"  scoped>
+.home-container{
+  .home-title{
     width: 100%;
     height: 100px;
-    line-height: 100px;
-    text-align: center;
-    font-size: 35px;
-    letter-spacing: 5px;
-  }
 
-  .home-title{
-    margin: 40px auto 20px;
-    width: 940px;
-    border-bottom: 5px solid #456d44;
-    font-size: 19px;
-    font-weight: 700;
-    letter-spacing: 3px;
   }
-  .home-content{
-    width: 100%;
-    margin: 0 auto;
-    text-indent:35px
-  }
+}
 </style>
